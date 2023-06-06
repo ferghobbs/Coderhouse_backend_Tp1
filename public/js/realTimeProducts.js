6f187bd2e1be977ea3f77110614f91a26c70f796
Your Name <fgomezhobbs@gmail.com>
2023-06-05 23:05:07 -0300

    Con boton de delete agregado a cada producto

--- public/js/realTimeProducts.js
+++ public/js/realTimeProducts.js
@@ -11,9 +11,47 @@ socket.on("prods", (prods) => {
     .map((prod, inde) => {
       return `<div>
             <em> ID=${prod.id} - ${prod.title} - ${prod.description}: $${prod.price} stock: ${prod.stock} </em>
+            <button onclick=eliminarProd(${prod.id})>Eliminar producto</button>
             </div>`;
     })
     .join(" ");
 
   document.getElementById("prods").innerHTML = html;
+});
+//Eliminar automatico
+function eliminarProd(id) {
+  fetch(`product/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Item deleted successfully");
+      } else {
+        console.log("Error deleting item");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+//Eliminar generico
+const deleteForm = document.getElementById("deleteForm");
+
+deleteForm.addEventListener("submit", (event) => {
+  const deleteId = document.getElementById("idDel").value;
+  event.preventDefault();
+  fetch(`${deleteForm.action}/${deleteId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Item deleted successfully");
+      } else {
+        console.log("Error deleting item");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
